7271b86f8429017b2b84ed99d5d16d02cec8cff1
Harry <harrychenhc@icloud.com>
2021-02-12 16:58:43 -0500

    finish addition functionality

--- main.js
+++ main.js
@@ -1,106 +1,153 @@
+const ADD = "ADD";
+const SUBTRACT = "SUBTRACT";
+const MULTIPLY = "MULTIPLY";
+const DIVIDE = "DIVIDE";
+
 class Calculator {
   initalVal = 0;
-  _tempVal = "";
-  ADD = "ADD";
-  SUBTRACT = "SUBTRACT";
-  MULTIPLY = "MULTIPLY";
-  DIVIDE = "DIVIDE";
-  lastOperation = null;
+  _firstVal = "";
+  _secondVal = "";
+  _operator = null;
 
   constructor(){
     this._value = this.initalVal;
   }
 
-  // concats the digit in string value
   loadValue(digit){
-    this._tempVal += digit; 
-
-  }
-
-  _resetTempVal(){
-    this._tempVal = "";
+    if(this.operator === null){
+      this._firstVal += digit;
+    } else {
+      this._secondVal += digit;
+    }
   }
 
   get value(){
     return this._value;
   }
 
-  get tempVal(){
-    return this._tempVal;
+  get firstVal(){
+    return this._firstVal;
   }
 
-  add(){
-    this._value += Number(this.tempVal);
-    this._resetTempVal();
-    return this.value;
+  get secondVal(){
+    return this._secondVal;
   }
 
-  subtract(){
-    this._value -= Number(this.tempVal);
-    this._resetTempVal();
-    return this.value;
+  get operator(){
+    return this._operator;
   }
 
-  multiply(){
-    this._value *= Number(this.tempVal);
-    this._resetTempVal();
-    return this.value;
-  }
-
-  divide(){
-    this._value /= Number(this.tempVal);
-    this._resetTempVal();
-    return this.value;
-  }
-
-  powerOfTwo(){
-    this._tempVal **= 2;
-    this.value = this.tempVal;
-    this._resetTempVal();
-    return this.value;
-  }
-
-  equal(){
-    const { ADD, SUBTRACT, DIVIDE, MULTIPLY, lastOperation, add, subtract, multiply, divide } = this;
-    if(lastOperation === null) return; 
-
-    switch(lastOperation){
+  set operation(op){
+    if(this._firstVal === ""){
+      this._operator = null;
+      return;
+    }
+    switch(op){
       case ADD:
-        add();
+        this._operator = ADD;
+        break;
       case SUBTRACT:
-        subtract();
+        this._operator = SUBTRACT;
+        break;
       case MULTIPLY:
-        multiply();
+        this._operator = MULTIPLY;
+        break;
       case DIVIDE:
-        divide();
+        this._operator = DIVIDE;
+        break;
       default:
-        return;
+        this._operator = null;
     }
   }
-  
+
+  compute(){
+    const { operator, firstVal, secondVal } = this;
+    if(secondVal === "" || operator === null) return this.value;
+    console.log(operator);
+    console.log(firstVal);
+    console.log(secondVal);
+    switch(operator){
+      case ADD:
+        this._value = Number(firstVal) + Number(secondVal);
+        break;
+      case SUBTRACT:
+        this._operator = SUBTRACT;
+        break;
+      case MULTIPLY:
+        this._operator = MULTIPLY;
+        break;
+      case DIVIDE:
+        this._operator = DIVIDE;
+        break;
+      default:
+        this._operator = null;
+    }
+    this.operation = null;
+    this._secondVal = "";
+    this._firstVal = this.value.toString();
+    console.log(this._firstVal);
+    return this.value;
+  }
+
   clear(){
     this._value = this.initalVal;
+    this._firstVal = "";
+    this._secondVal = "";
   }
 }
 
 const calc = new Calculator();
 
-const btnCollection = document.getElementsByClassName('btn-num');
+// Loads all number buttons' values into tempVal
+const btnCollection = document.getElementsByClassName("btn-num");
 for(const btn of btnCollection){
   btn.addEventListener("click", () => {
     calc.loadValue(btn.value);
-    document.getElementById('display').value = calc.tempVal;
+    if(calc.operator){
+      document.getElementById("display").value = calc.secondVal;
+    } else {
+      document.getElementById("display").value = calc.firstVal;
+    }
   })
 }
 
+// Add button
 document.getElementById('add').addEventListener('click', () => {
-  const newVal = calc.add();
+  /* 
+  if there was already a "secondVal" when add is clicked, then we just automatically 
+  call compute. Else, we just end the function call there.
+  */
+  calc.operation = ADD;
+  if(calc.secondVal === "") return;
+  const newVal = calc.compute();
   document.getElementById('display').value = newVal;
+})
+
+// Subtract button
+document.getElementById('subtract').addEventListener('click', () => {
+
+})
+
+// Multiply button
+document.getElementById('multiply').addEventListener('click', () => {
+  if(calc.operator !== MULTIPLY){
+    calc.operation = MULTIPLY;
+    return;
+  }
+})
+
+// Divide button
+document.getElementById('divide').addEventListener('click', () => {
+  if(calc.operator !== DIVIDE){
+    calc.operation = DIVIDE;
+    return;
+  }
 })
 
 // Equal button
 document.getElementById('equal').addEventListener('click', () => {
-  
+  const newVal = calc.compute();
+  document.getElementById('display').value = newVal;
 })
 
 // Reset button
